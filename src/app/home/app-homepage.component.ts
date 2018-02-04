200f50c09e2a49dce81c9749a2c08164a0c3823c
Anthony Soto <mr.anthonygsoto@gmail.com>
2018-02-04 04:20:19 -0800

    Update search bar and store user input

--- src/app/home/app-homepage.component.ts
+++ src/app/home/app-homepage.component.ts
@@ -5,11 +5,18 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-home',
   templateUrl: 'app-homepage.component.html',
-  styleUrls: ['../../../node_modules/bootstrap/dist/css/bootstrap.css']
+  styleUrls: ['../../../node_modules/bootstrap/dist/css/bootstrap.css',
+    'app-homepage.component.css']
 })
 export class HomePageComponent implements OnInit {
 
   constructor(private _router: Router) { }
+
+  userInput = '';
+  onEnter(userInput: string) { 
+    this.userInput = userInput;
+    console.log(userInput);
+  }
 
   ngOnInit() {
   }
